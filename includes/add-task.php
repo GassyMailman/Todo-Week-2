acf9f1c013d6dbbe3331d85f8131089c37da8f17
Hayden Guthrie <seehaydeng@gmail.com>
2015-04-24 18:53:22 -0700

    Milestone 17 & 18

--- includes/add-task.php
+++ includes/add-task.php
@@ -5,7 +5,7 @@
 	$time = date('H:i:s');
 	
 	include('connect.php');
-	$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 	$mysqli->query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");
 	$query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time' ";
 	if ($result = $mysqli->query($query)) {
