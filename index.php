6d6cc4022d5b0fc6ef61429fb94042fbf55d4b26
Hayden Guthrie <seehaydeng@gmail.com>
2015-04-24 18:39:21 -0700

    Milestone 9

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
+
 <html>
 	<head>
-		<title> Hayden's To-Do List 2 </title>
+		<title> Julian's To-Do List </title>
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 	</head>
 	<body>
 		<div class="wrap">
-		<div class="task-list">
+			<div class="task-list">
 				<ul>
 					<?php require("includes/connect.php"); ?>
 				</ul>
@@ -16,4 +17,20 @@
 			</form>
 		</div>
 	</body>
- </html> 		 
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script>
+		add_task();
+		function add_task() {
+			$('.add-new-task').submit(function() {
+				var new_task = $('.add-new-task input[name=new-task]').val();
+				if (new_task != '') {
+					$.post('includes/add-task.php', { task: new_task }, function (data) {
+						$('add-new-task input[name=new-task]').val();
+						$(data).appendTo('task-list ul').hide.fadeIn();
+					});
+				}
+				return false;
+			});
+		}
+	</script>
+</html>
